1e9276ec63dc061cd3cfd40ac3ebf66c6a523c02
danyal-deekoders <deekoders@gmail.com>
2024-11-11 22:57:43 +0500

    Update image-picker.js

--- components/meals/image-picker/image-picker.js
+++ components/meals/image-picker/image-picker.js
@@ -1,17 +1,21 @@
 "use client";
+
 import { useRef, useState } from "react";
-import classes from "./image-picker.module.css";
 import Image from "next/image";
 
-function ImagePicker({ label, name }) {
-  const [pickedImage, setPickedImage] = useState();
-  const imageInputRef = useRef();
+import classes from "./image-picker.module.css";
 
-  const handleClick = () => {
-    imageInputRef.current.click();
-  };
-  const handleImageChange = (event) => {
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
     const file = event.target.files[0];
+
     if (!file) {
       return;
     }
@@ -21,31 +25,41 @@ function ImagePicker({ label, name }) {
     fileReader.onload = () => {
       setPickedImage(fileReader.result);
     };
+
     fileReader.readAsDataURL(file);
-  };
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
-      <div classNam e={classes.controls}>
+      <div className={classes.controls}>
         <div className={classes.preview}>
-          {!pickedImage && "No Image Selected"}
-          {pickedImage && <Image src={pickedImage} fill alt='Selected Image' />}
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt='The image selected by the user.'
+              fill
+            />
+          )}
         </div>
         <input
           className={classes.input}
           type='file'
-          ref={imageInputRef}
           id={name}
           accept='image/png, image/jpeg'
           name={name}
+          ref={imageInput}
           onChange={handleImageChange}
         />
-        <button className={classes.button} type='button' onClick={handleClick}>
+        <button
+          className={classes.button}
+          type='button'
+          onClick={handlePickClick}
+        >
           Pick an Image
         </button>
       </div>
     </div>
   );
 }
-
-export default ImagePicker;
